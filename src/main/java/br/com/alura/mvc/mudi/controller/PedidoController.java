9383da875f920498deb003bb3d1953f6941d9b02
carlosscamilo <carlos_camilo250@hotmail.com>
2022-03-07 16:24:19 -0300

    Segunda Versão do MudiApp

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -3,6 +3,7 @@ package br.com.alura.mvc.mudi.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.User;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import br.com.alura.mvc.mudi.repository.UserRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
-	
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
@@ -24,17 +30,22 @@ public class PedidoController {
 	public String formulario(RequisicaoNovoPedido requisicao) {
 		return "pedido/formulario";
 	}
-	
+
 	@PostMapping("novo")
 	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
 		if (result.hasErrors()) {
 			return "pedido/formulario";
 		}
-		
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = userRepository.findByUsername(username);
+
 		Pedido pedido = requisicao.toPedido();
+		pedido.setUser(user);
+
 		pedidoRepository.save(pedido);
-		
-		return "redirect:/home";
+
+		return "redirect:/usuario/pedido";
 	}
-	
+
 }
--- src/main/java/br/com/alura/mvc/mudi/model/StatusPedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/StatusPedido.java
@@ -1,5 +1,8 @@
 package br.com.alura.mvc.mudi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public enum StatusPedido {
 	AGUARDANDO, APROVADO, ENTREGUE
 }
